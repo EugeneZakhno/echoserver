1e23e679163a5b50f8f3ee97b4d24f88b106944b
EugeneZakhno <zohan1234567@gmail.com>
2021-12-29 21:34:56 +0300

    init

---
+++ src/main/java/ru/intervale/courses/echoserver/controller/Controller.java
@@ -0,0 +1,15 @@
+package ru.intervale.courses.echoserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello () {
+        return "hello";
+    }
+
+
+
+}
