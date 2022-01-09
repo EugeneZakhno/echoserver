abeda7f6cb374356c26db895795b2c96178f0d8d
Eugene Zakhno <zohan1234567@gmail.com>
2022-01-09 04:04:25 +0300

    COURSES-2: Added all methods for task 2

--- src/main/java/ru/intervale/courses/echoserver/controller/MainController.java
+++ src/main/java/ru/intervale/courses/echoserver/controller/MainController.java
@@ -1,26 +1,81 @@
 package ru.intervale.courses.echoserver.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
 
 @RestController
 public class MainController {
 
+    //http://localhost:8080/hello
     @GetMapping("/hello")
-    public String hello () {
-        return "hello, Spring";
+    public String hello() {
+        return "Hello!";
     }
 
-    @GetMapping("/withParams/{param}")
-    public String withParams (@RequestParam String param) {
-        return "withParams, spring " + param;
+    //http://localhost:8080/withParams?param=jesus
+    @GetMapping(value = "withParams")
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    public String withParams(@RequestParam("param") String param) {
+        return param;
     }
 
-    @GetMapping("/withPathVariable/{param}")
-    public String withPathVariable (@PathVariable String id) {
-        return "withParams, spring " + id;
+    //http://localhost:8080/withPathVariable/12
+    @GetMapping("/withPathVariable/{id}")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public String withPathVariable(@PathVariable Long id) {
+        return String.valueOf(id);
     }
 
+
+    @PostMapping(value = "/echo")
+    public ResponseEntity<String> echo(@RequestHeader Map<String, String> headers, @RequestBody String textBody) {
+        if (headers.containsValue("application/json")) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .body(textBody);
+        } else if (headers.containsValue("application/xml")) {
+            return ResponseEntity
+                    .ok()
+                    .header(HttpHeaders.CONTENT_TYPE, "application/xml")
+                    .body(textBody);
+        } else {
+            return ResponseEntity.ok().body("Wrong Content-Type");
+        }
+    }
+
+    @PostMapping(value = "/echoxml", produces = "application/xml")
+    public String echoXml(@RequestBody String xmlText) {
+        return xmlText;
+    }
+
+    @PutMapping(value = "/put")
+    public String echoPut() {
+        return "";
+    }
+
+    @GetMapping("/cookie")
+    public ResponseEntity<?> cookie(@CookieValue(name = "Date", defaultValue = "firstRequest") String cookieName) {
+        if (cookieName.equals("firstRequest")) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.SET_COOKIE, String.valueOf(ResponseCookie.from("Date", "" +
+                            new SimpleDateFormat("yyyy.MM.dd-HH:mm:ss").format(Calendar.getInstance().getTime()))
+                            .build()))
+                    .build();
+        } else {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.SET_COOKIE, cookieName)
+                    .header(HttpHeaders.SET_COOKIE, String.valueOf(ResponseCookie.from("Date", "" +
+                            new SimpleDateFormat("yyyy.MM.dd-HH:mm:ss").format(Calendar.getInstance().getTime()))
+                            .build()))
+                    .build();
+        }
+
+    }
 }
